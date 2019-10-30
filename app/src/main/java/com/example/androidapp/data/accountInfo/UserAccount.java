1e0278ea7bbcbe8a7c673089f5f09a3d959617af
Brock Maxwell <bmaxwellcsu@gmail.com>
2019-10-30 10:17:50 -0400

    Added device creation pages

---
+++ app/src/main/java/com/example/androidapp/data/accountInfo/UserAccount.java
@@ -0,0 +1,106 @@
+package com.example.androidapp.data.accountInfo;
+
+public class UserAccount {
+    private String acctID;
+    private String accountType;
+
+    private String username;
+    private String password;
+
+    private String creationDate;
+
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String address;
+    private String email;
+    private String phoneNumber;
+
+    public String getAcctID() {
+        return acctID;
+    }
+
+    public void setAcctID(String acctID) {
+        this.acctID = acctID;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
