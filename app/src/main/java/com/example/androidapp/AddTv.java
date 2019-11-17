e55e9d6fd808549abab5b29ae8572d884a34e6e6
Bonnie Williams <boxerpuplovr@gmail.com>
2019-11-17 00:29:57 -0500

    altered television activity layouts and began altering AddTv.java OnClick

--- app/src/main/java/com/example/androidapp/AddTv.java
+++ app/src/main/java/com/example/androidapp/AddTv.java
@@ -2,7 +2,11 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddTv extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class AddTv extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tv);
+
+        Button createTvButton = (Button)findViewById(R.id.createTvButton);
+        final EditText tvName = (EditText)findViewById(R.id.enterTvName);
+        final EditText tvLocation = (EditText)findViewById(R.id.enterTvLocation);
+
+
+        createTvButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //create new TV object
+                com.example.androidapp.data.deviceInfo.Television tv =
+                        new com.example.androidapp.data.deviceInfo.Television(tvName.getText().toString(), tvLocation.getText().toString());
+
+                //go to TV settings
+                Intent tvSettings = new Intent(getApplicationContext(), Television.class);
+                startActivity(tvSettings);
+            }
+        });
+
     }
 }
